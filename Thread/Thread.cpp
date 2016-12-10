7cb0e1d203fa5182a905ae6145c1d2732cd9672f
R.J.V. Bertin <rjvbertin@gmail.com>
2016-12-10 01:03:15 +0100

    Thread: fix missing argument issue

--- Thread/Thread.cpp
+++ Thread/Thread.cpp
@@ -361,7 +361,7 @@ bool Thread::Cancel()
 	if( i == 5 ){
 #ifdef DEBUG
 		fprintf( stderr, "@@ %p->Cancel() thread %p didn't cancel in %ds\n",
-			this, i );
+			this, m_ThreadCtx.m_hThread, i );
 #endif //DEBUG
 		ret = false;
 	}

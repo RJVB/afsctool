2900c0da4a44ec7e93039e810fea77264b8b368b
R.J.V. Bertin <rjvbertin@gmail.com>
2018-04-10 18:33:48 +0200

    Search for zlib via pkgconfig first, falling back to cmake's own routine if nothing is found that way.

---
+++ cmake/modules/FindZLIBP.cmake
@@ -0,0 +1,40 @@
+set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
+
+include(FindPkgConfig)
+include(FindPackageHandleStandardArgs)
+include(FeatureSummary)
+
+pkg_check_modules(PKG_ZLIBP zlib)
+
+set(ZLIBP_DEFINITIONS ${PKG_ZLIBP_CFLAGS_OTHER})
+set(ZLIBP_VERSION ${PKG_ZLIBP_VERSION})
+set(ZLIBP_INCLUDE_DIR ${PKG_ZLIBP_INCLUDEDIR})
+set(ZLIBP_LIBRARIES ${PKG_ZLIBP_LIBRARIES})
+
+if(NOT PKG_ZLIBP_FOUND)
+	message(STATUS "zlib was not found with pkg-config, falling back to cmake's find routine")
+     # find via cmake's standard FindZLIB
+	find_package(ZLIB)
+	set(PKG_ZLIBP_FOUND ${ZLIB_FOUND})
+	set(ZLIBP_VERSION ${ZLIB_VERSION_STRING})
+	set(ZLIBP_INCLUDE_DIR ${ZLIB_INCLUDE_DIR})
+	set(ZLIBP_INCLUDE_DIRS ${ZLIB_INCLUDE_DIRS})
+	set(ZLIBP_LIBRARIES ${ZLIB_LIBRARIES})
+endif()
+
+find_package_handle_standard_args(ZLIBP
+    FOUND_VAR
+        ZLIBP_FOUND
+    REQUIRED_VARS
+        ZLIBP_INCLUDE_DIR
+    VERSION_VAR
+        ZLIBP_VERSION
+)
+
+mark_as_advanced(ZLIBP_INCLUDE_DIR)
+
+set_package_properties(ZLIBP PROPERTIES
+                      DESCRIPTION "zlib compression library"
+                      TYPE REQUIRED
+                      URL "http://www.zlib.net/")
+

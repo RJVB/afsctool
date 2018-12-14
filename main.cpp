ad5ac3bd236007012a103fb7768de5cbd0daf6c3
R.J.V. Bertin <rjvbertin@gmail.com>
2018-12-14 23:04:23 +0100

    set a few LaunchServices properties

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "afsctool.h"
 
+#include <CoreFoundation/CoreFoundation.h>
+
 int main (int argc, const char * argv[])
 {
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    if (mainBundle) {
+        // get the application's Info Dictionary. For app bundles this would live in the bundle's Info.plist,
+        // for regular executables it is obtained in another way.
+        CFMutableDictionaryRef infoDict = (CFMutableDictionaryRef) CFBundleGetInfoDictionary(mainBundle);
+        if (infoDict) {
+            CFDictionarySetValue(infoDict, CFSTR("CFBundleIdentifier"), CFSTR("org.brkirch.afsctool"));
+            CFDictionarySetValue(infoDict, CFSTR("CFBundleName"), CFSTR("AFSCtool"));
+            CFDictionarySetValue(infoDict, CFSTR("CFBundleDisplayName"), CFSTR("AFSCtool"));
+            CFDictionarySetValue(infoDict, CFSTR("NSAppSleepDisabled"), CFSTR("1"));
+            CFDictionarySetValue(infoDict, CFSTR("NSSupportsAutomaticTermination"), CFSTR("0"));
+        }
+    }
     return afsctool(argc, argv);
 }

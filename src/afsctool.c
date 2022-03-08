94eb06da30cc61f2381d125f5c19d59bc66b9400
Dmitriy Polomin <dpolomin@gmail.com>
2022-03-09 02:13:26 +0300

    O_NONBLOCK for 'open' as workaround for freezing

--- src/afsctool.c
+++ src/afsctool.c
@@ -479,7 +479,7 @@ void compressFile(const char *inFile, struct stat *inFileInfo, struct folder_inf
 	}
 #endif
 	// use open() with an exclusive lock so noone can modify the file while we're at it
-	fdIn = open(inFile, O_RDWR|O_EXLOCK);
+	fdIn = open(inFile, O_RDWR|O_EXLOCK|O_NONBLOCK);
 	if (fdIn == -1)
 	{
 		fprintf(stderr, "%s: %s\n", inFile, strerror(errno));
